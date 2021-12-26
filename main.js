0b39a815601ff8b4f9ca7dc88d9ef55f9cf55fb9
jinsoo49 <jinsoooo49@gmail.com>
2021-12-26 15:14:37 +0900

    create project

---
+++ main.js
@@ -0,0 +1,13 @@
+const str = `
+010-1234-5678
+jinsoooo49@gmail.com
+https://www.ombdapi.com/?apikey=7035c60
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`;
+
+// g, i플래그
+// const regexp = new RegExp("the", "gi");
+// const regexp = /the/gi; 동일
+
+console.log(str.match(regexp));
